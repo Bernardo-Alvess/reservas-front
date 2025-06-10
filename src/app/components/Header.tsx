2b801789d99a43457127d069080e01b048bd0d73
Bernardo <bernardoalves330@gmail.com>
2025-06-10 19:41:51 -0300

    chor: cleanup

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,55 +1,5 @@
 'use client';
 
-// import Link from 'next/link';
-// import { useQuery } from '@tanstack/react-query';
-// import { useLogin } from '../hooks/useLogin';
-// import { useUserContext } from '../context/user/useUserContext';
-
-// export default function Header() {
-//   const { user } = useUserContext();
-//   const { logout } = useLogin('client');
-
-//   return (
-//     <header className="bg-white shadow-sm border-b border-gray-200">
-//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
-//         <Link href="/" className="text-xl font-bold text-gray-800 hover:text-blue-600 transition-colors">
-//           ReservasApp
-//         </Link>
-
-//         <nav className="flex items-center gap-6 text-sm font-medium text-gray-600">
-//           <Link href="/home" className="hover:text-blue-600 transition-colors">
-//             Home
-//           </Link>
-//           {user && (
-//             <Link href="/my-reserves" className="hover:text-blue-600 transition-colors">
-//               Minhas Reservas
-//             </Link>
-//           )}
-//           <Link href="/dashboard" className="hover:text-blue-600 transition-colors">
-//             Painel Admin
-//           </Link>
-//           <p>
-//             |
-//           </p>
-//           {!user ? (
-//             <>
-//               <Link href="/login" className="hover:text-blue-600 transition-colors">
-//                 Entrar
-//               </Link>
-//             </>
-//           ) : (
-//             <button className="hover:text-blue-600 transition-colors" onClick={logout}>
-//               Sair
-//             </button>
-//           )}
-    
-//         </nav>
-//       </div> 
-//     </header>
-//   );
-// }
-
-
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { User, Settings, Calendar } from "lucide-react";
