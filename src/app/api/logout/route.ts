ed0a7db7469d4a989e61e787b997ff1419ce021d
Bernardo <bernardoalves330@gmail.com>
2025-06-16 21:45:02 -0300

    feat: fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,15 +3,18 @@ import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   // Pega a URL de origem da requisição
-  const origin = request.headers.get('origin') || 'http://localhost:3000';
+  const origin = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
   const response = NextResponse.redirect(new URL('/home', origin));
 
   response.cookies.set({
     name: 'sessionToken',
     value: '',
     path: '/',
-    httpOnly: true,
     maxAge: 0,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    // domain: aqui você pode definir se necessário, mas tente remover primeiro pra evitar conflito
   });
 
   return response;
