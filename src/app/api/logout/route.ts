b14b4f266c2720cbfcfc8b1d487e09d9912065fa
Bernardo <bernardoalves330@gmail.com>
2025-06-16 21:53:51 -0300

    testse

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   // Pega a URL de origem da requisição
   const origin = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
   const response = NextResponse.redirect(new URL('/home', origin));
@@ -12,9 +12,8 @@ export async function GET(request: NextRequest) {
     path: '/',
     maxAge: 0,
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    // domain: aqui você pode definir se necessário, mas tente remover primeiro pra evitar conflito
+    secure: process.env.NEXT_PUBLIC_ENVIRONMENT === 'production',
+    sameSite: process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' ? 'none' : 'lax',
   });
 
   return response;
